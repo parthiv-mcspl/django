de12e4602d69197f45d0c5158283158cfa628eea
parthiv-mcspl <113373062+parthiv-mcspl@users.noreply.github.com>
2025-03-13 15:46:01 +0530

    Update settings.py

--- collab_site/settings.py
+++ collab_site/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-6w&3#f(igatr1lnx#k+85xb+r5kc!$fafo%vnu*!j1w$^uiecc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['creview-collabsite.onrender.com']
+
 
 
 # Application definition
